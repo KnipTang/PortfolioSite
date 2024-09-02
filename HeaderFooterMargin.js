5bbbc6ea9d3f7a49ec756badf0c0d64f6760b8ff
KnipTang <arne.olemans@telenet.be>
2024-09-02 14:04:18 +0200

    Added mobile support 90% done

---
+++ HeaderFooterMargin.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', adjustBodyMargins);
+window.addEventListener('resize', adjustBodyMargins);
+
+function adjustBodyMargins() {
+    const headerHeight = document.querySelector('.header').offsetHeight;
+    const footerHeight = document.querySelector('.footer').offsetHeight;
+
+    const siteBody = document.querySelector('.site-body');
+    const footerLinksLinks = document.querySelectorAll('.footer-Links img.button-icon');
+    
+    siteBody.style.marginTop = `${headerHeight}px`;
+    siteBody.style.marginBottom = `${footerHeight}px`;
+
+    footerLinksLinks.forEach(img => {
+        img.style.height = `${footerHeight/1.5}px`;
+        img.style.width = 'auto';
+    });
+}
